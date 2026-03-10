c28f46bcae9632c0a8731338ce94c86db2d30691
mateovillar2501 <mativial04@gmail.com>
2026-03-10 14:38:00 -0600

    Emilio

--- AcademiaStemTapachula/obj/Debug/net8.0/AcademiaStemTapachula.AssemblyInfo.cs
+++ AcademiaStemTapachula/obj/Debug/net8.0/AcademiaStemTapachula.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0e71e55be27243d856a14f42ae4c7f44186dd67f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64b24cbc134d501308c33b01a88bb6847587ce8d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
