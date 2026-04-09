54711b1247e516bcb52a9d3d45e619716e3fb6f6
mateovillar2501 <mativial04@gmail.com>
2026-04-09 14:12:58 -0600

    CREACION DE ARCHIVOS PARA BACKEND

--- AcademiaStemTapachula/obj/Debug/net8.0/AcademiaStemTapachula.AssemblyInfo.cs
+++ AcademiaStemTapachula/obj/Debug/net8.0/AcademiaStemTapachula.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f9da37b5d1a3b811bc5d1353704da5e6d8f4b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99959a0760fc1cca9fc86c25858f77bdb8b80c5e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AcademiaStemTapachula")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ AcademiaStemTapachula/wwwroot/dashboard/js/App/Cursos/AgregarCurso.js
@@ -0,0 +1,38 @@
+﻿//MODULO DE CONFIGURACIÓN PARA AGREGAR UN CURSO
+
+function renderCode() {
+
+    const code = document.getElementById("codeEditor").value;
+    const frame = document.getElementById("previewFrame");
+
+    const doc = frame.contentDocument || frame.contentWindow.document;
+
+    doc.open();
+    doc.write(code);
+    doc.close();
+}
+
+function clearEditor() {
+
+    location.reload();
+}
+
+
+/* Permitir TAB dentro del textarea como editor real */
+
+document.getElementById("codeEditor").addEventListener("keydown", function (e) {
+
+    if (e.key === "Tab") {
+        e.preventDefault();
+
+        let start = this.selectionStart;
+        let end = this.selectionEnd;
+
+        this.value = this.value.substring(0, start)
+            + "\t"
+            + this.value.substring(end);
+
+        this.selectionStart = this.selectionEnd = start + 1;
+    }
+
+});
